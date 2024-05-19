5a5a3189fcfc29d0985a07880837aac3153d8d2c
KaiN <kain@piwnica.ws>
2024-05-19 08:01:26 +0200

    clean up TrackAnimator, use its wheel list in TrackedRaycastVehicleController

--- Scripts/TrackAnimator.gd
+++ Scripts/TrackAnimator.gd
@@ -1,9 +1,10 @@
+class_name TrackAnimator
 extends MeshInstance3D
 
 # public variables
-@export var roadWheelPaths : Array[NodePath]
-@export var sprocketPath: NodePath
-@export var idlerPath: NodePath
+@export var road_wheels : Array[DriveElement]
+@export var sprocket : MeshInstance3D
+@export var idler : MeshInstance3D
 @export var wheelSpeedScaling : float = 1.0
 @export var sprocketSpeedScaling : float = 1.6
 @export var idlerSpeedScaling : float = 1.5
@@ -11,39 +12,31 @@ extends MeshInstance3D
 @export var animate_track_texture := true
 
 # private variables
-var roadWheels : Array
-var sprocket : MeshInstance3D
-var idler : MeshInstance3D
-var trackMat : StandardMaterial3D
-var lastPos : Vector3 = Vector3()
+var track_material : StandardMaterial3D
+var last_position := Vector3.ZERO
 
 func _ready() -> void:
 	# setup references
-	sprocket = get_node(sprocketPath)
-	idler = get_node(idlerPath)
-	for wheel in roadWheelPaths:
-		roadWheels.append(get_node(wheel))
-	trackMat = mesh.surface_get_material(0)
+	track_material = mesh.surface_get_material(0)
 
-func _physics_process(delta) -> void:
+func _physics_process(delta: float) -> void:
 	# obtain velocity of the track
-	var instantV = (global_transform.origin - lastPos) / delta
-	var ZVel = (instantV * global_transform.basis).z
-	lastPos = global_transform.origin
+	var instant_velocity := (global_transform.origin - last_position) / delta
+	var velocity_z := (instant_velocity * global_transform.basis).z
+	last_position = global_transform.origin
 
 	# animate wheels
-	# NOTE: The rotation is bugged in 3.1, it's fixed in 3.2 Beta 1
-	for wheel in roadWheels:
-		wheel.rotate_x(ZVel * wheelSpeedScaling * delta)
+	for wheel: DriveElement in road_wheels:
+		wheel.rotate_x(velocity_z * wheelSpeedScaling * delta)
 
 	# animate drive sprocket and idler
-	sprocket.rotate_x(ZVel * sprocketSpeedScaling * delta)
-	idler.rotate_x(ZVel * idlerSpeedScaling * delta)
+	sprocket.rotate_x(velocity_z * sprocketSpeedScaling * delta)
+	idler.rotate_x(velocity_z * idlerSpeedScaling * delta)
 
 	if animate_track_texture:
 		# animate track texture
-		trackMat.uv1_offset.y += (ZVel * trackUVScaling) * delta
+		track_material.uv1_offset.y += (velocity_z * trackUVScaling) * delta
 
 		# clamp UV offset of tracks
-		if trackMat.uv1_offset.y > 1.0 or trackMat.uv1_offset.y < -1.0:
-			trackMat.uv1_offset.y = 0.0
+		if track_material.uv1_offset.y > 1.0 or track_material.uv1_offset.y < -1.0:
+			track_material.uv1_offset.y = 0.0
--- Scripts/TrackedRaycastVehicleController.gd
+++ Scripts/TrackedRaycastVehicleController.gd
@@ -22,8 +22,8 @@ enum DriveMode {
 
 @export var autoStopSpeedMS := 1.0
 
-@export var left_drive_elements: Array[DriveElement] = []
-@export var right_drive_elements: Array[DriveElement] = []
+@export var left_track: TrackAnimator
+@export var right_track: TrackAnimator
 
 var _drive_per_ray := enginePower
 var _current_drive_power := 0.0
@@ -33,78 +33,78 @@ var _last_steer_value := 0.0
 
 func _handle_physics(delta) -> void:
 	# get throttle and steering input
-	var forwardDrive := Input.get_axis("reverse", "forward")
+	var forward_drive := Input.get_axis("reverse", "forward")
 	var steering := Input.get_axis("steer_left", "steer_right")
 
 	# Invert steering when reversing if enabled
-	if forwardDrive < 0 && invertSteerWhenReverse:
+	if forward_drive < 0 && invertSteerWhenReverse:
 		steering *= - 1
 
 	# calculate speed interpolation
-	var speedInterp: float
+	var speed_interp: float
 	# forward, use forward max speed
-	if forwardDrive > 0:
-		speedInterp = remap(absf(_current_speed), 0.0, maxSpeedKph / 3.6, 0.0, 1.0)
+	if forward_drive > 0:
+		speed_interp = remap(absf(_current_speed), 0.0, maxSpeedKph / 3.6, 0.0, 1.0)
 	# reverse, use reverse max speed
-	elif forwardDrive < 0:
-		speedInterp = remap(absf(_current_speed), 0.0, maxReverseSpeedKph / 3.6, 0.0, 1.0)
+	elif forward_drive < 0:
+		speed_interp = remap(absf(_current_speed), 0.0, maxReverseSpeedKph / 3.6, 0.0, 1.0)
 	# steering drive (always at start of curve)
-	elif forwardDrive == 0 && steering != 0:
-		speedInterp = 0
+	elif forward_drive == 0 && steering != 0:
+		speed_interp = 0
 
 	# get force from torque curve (based on current speed)
-	_current_drive_power = torqueCurve.sample_baked(speedInterp) * _drive_per_ray
+	_current_drive_power = torqueCurve.sample_baked(speed_interp) * _drive_per_ray
 
 	# handle drive and braking for tracks depending on mode
 	if driveTrainMode == DriveMode.DOUBLE_DIFF:
 		# double differential setup (steer with control of drive force)
-		var leftForce := Vector3.ZERO
-		var rightForce := Vector3.ZERO
+		var left_force := Vector3.ZERO
+		var right_force := Vector3.ZERO
 		var braking := rollingResistance
 
 		# calculate drive forces
-		var LDriveFac := forwardDrive + steering
-		var RDriveFac := forwardDrive + steering * - 1
-		leftForce = global_transform.basis.z * _current_drive_power * LDriveFac
-		rightForce = global_transform.basis.z * _current_drive_power * RDriveFac
+		var left_drive_factor := forward_drive + steering
+		var right_drive_factor := forward_drive + steering * - 1
+		left_force = global_transform.basis.z * _current_drive_power * left_drive_factor
+		right_force = global_transform.basis.z * _current_drive_power * right_drive_factor
 
 		# no brakes during normal driving
-		if LDriveFac != 0 || RDriveFac != 0:
+		if left_drive_factor != 0 || right_drive_factor != 0:
 			braking = 0
 
 		# slow down if input opposite drive direction
-		if signf(_current_speed) != signf(forwardDrive):
-			braking = trackBrakePercent * absf(forwardDrive)
+		if signf(_current_speed) != signf(forward_drive):
+			braking = trackBrakePercent * absf(forward_drive)
 
 		# apply parking brake if sitting still
-		if forwardDrive == 0 && steering == 0 && absf(_current_speed) < autoStopSpeedMS:
+		if forward_drive == 0 && steering == 0 && absf(_current_speed) < autoStopSpeedMS:
 			braking = trackBrakePercent
 
 		# finally apply all forces and braking
-		for element in left_drive_elements:
-			element.apply_force(leftForce)
+		for element: DriveElement in left_track.road_wheels:
+			element.apply_force(left_force)
 			element.apply_brake(braking)
 
-		for element in right_drive_elements:
-			element.apply_force(rightForce)
+		for element: DriveElement in right_track.road_wheels:
+			element.apply_force(right_force)
 			element.apply_brake(braking)
 
 	elif driveTrainMode == DriveMode.BRAKED_DIFF:
 		# braked differential setup (steer with braking)
-		var driveForce := Vector3.ZERO
-		var leftBraking := rollingResistance
-		var rightBraking := rollingResistance
+		var drive_force := Vector3.ZERO
+		var left_braking := rollingResistance
+		var right_braking := rollingResistance
 
 		# calculate drive force
-		driveForce = global_transform.basis.z * _current_drive_power * forwardDrive
+		drive_force = global_transform.basis.z * _current_drive_power * forward_drive
 
 		# reset steering if opposite input used
 		if signf(steering) != signf(_last_steer_value):
 			_current_steer_brake_power = 0
 		# gradually increase steering brake and decay if no steering applied
 		if steering != 0:
-			var desiredBrakePower := absf(steering) * trackBrakePercent
-			_current_steer_brake_power = move_toward(_current_steer_brake_power, desiredBrakePower, trackBrakingSpeed * delta)
+			var target_brake_power := absf(steering) * trackBrakePercent
+			_current_steer_brake_power = move_toward(_current_steer_brake_power, target_brake_power, trackBrakingSpeed * delta)
 		else:
 			_current_steer_brake_power = move_toward(_current_steer_brake_power, 0, trackBrakingSpeed * delta)
 		# set last steer value
@@ -112,62 +112,62 @@ func _handle_physics(delta) -> void:
 
 		# calculate steering brake
 		if steering < 0:
-			leftBraking = _current_steer_brake_power * absf(steering)
-			rightBraking = 0
+			left_braking = _current_steer_brake_power * absf(steering)
+			right_braking = 0
 		else:
-			leftBraking = 0
-			rightBraking = _current_steer_brake_power * absf(steering)
+			left_braking = 0
+			right_braking = _current_steer_brake_power * absf(steering)
 
 		# slow down if input opposite drive direction
-		if signf(_current_speed) != signf(forwardDrive):
-			leftBraking = trackBrakePercent * absf(forwardDrive)
-			rightBraking = trackBrakePercent * absf(forwardDrive)
+		if signf(_current_speed) != signf(forward_drive):
+			left_braking = trackBrakePercent * absf(forward_drive)
+			right_braking = trackBrakePercent * absf(forward_drive)
 
 		# apply parking brake if sitting still
-		if forwardDrive == 0 && steering == 0 && absf(_current_speed) < autoStopSpeedMS:
-			leftBraking = trackBrakePercent
-			rightBraking = trackBrakePercent
+		if forward_drive == 0 && steering == 0 && absf(_current_speed) < autoStopSpeedMS:
+			left_braking = trackBrakePercent
+			right_braking = trackBrakePercent
 
 		# finally apply all forces and braking
-		for element in left_drive_elements:
-			element.apply_force(driveForce)
-			element.apply_brake(leftBraking)
+		for element: DriveElement in left_track.road_wheels:
+			element.apply_force(drive_force)
+			element.apply_brake(left_braking)
 
-		for element in right_drive_elements:
-			element.apply_force(driveForce)
-			element.apply_brake(rightBraking)
+		for element: DriveElement in right_track.road_wheels:
+			element.apply_force(drive_force)
+			element.apply_brake(right_braking)
 
 	elif driveTrainMode == DriveMode.DIRECT_FORCES:
 		# Drive and turn using direct forces on vehicle body
 		# recalculate engine power
-		if signf(_current_speed) != signf(forwardDrive):
-			speedInterp = 0
-		_current_drive_power = torqueCurve.interpolate_baked(speedInterp) * enginePower / 2
+		if signf(_current_speed) != signf(forward_drive):
+			speed_interp = 0
+		_current_drive_power = torqueCurve.interpolate_baked(speed_interp) * enginePower / 2
 
 		# calculate track forces
-		var leftDriveForce := global_transform.basis.z * _current_drive_power * (forwardDrive + steering)
-		var rightDriveForce := global_transform.basis.z * _current_drive_power * (forwardDrive + steering * - 1)
+		var left_drive_force := global_transform.basis.z * _current_drive_power * (forward_drive + steering)
+		var right_drive_force := global_transform.basis.z * _current_drive_power * (forward_drive + steering * - 1)
 
 		# check grounded status
-		var leftTrackGrounded := false
-		var rightTrackGrounded := false
-		for element in left_drive_elements:
+		var is_left_track_grounded := false
+		var is_right_track_grounded := false
+		for element: DriveElement in left_track.road_wheels:
 			if element.grounded:
-				leftTrackGrounded = true
-		for element in right_drive_elements:
+				is_left_track_grounded = true
+		for element: DriveElement in right_track.road_wheels:
 			if element.grounded:
-				rightTrackGrounded = true
+				is_right_track_grounded = true
 
 		# apply track forces
-		if leftTrackGrounded:
-			apply_force(leftDriveForce, to_global(Vector3(1.5, -0.2, 0)) - global_transform.origin)
-		if rightTrackGrounded:
-			apply_force(rightDriveForce, to_global(Vector3( - 1.5, -0.2, 0)) - global_transform.origin)
+		if is_left_track_grounded:
+			apply_force(left_drive_force, to_global(Vector3(1.5, -0.2, 0)) - global_transform.origin)
+		if is_right_track_grounded:
+			apply_force(right_drive_force, to_global(Vector3( - 1.5, -0.2, 0)) - global_transform.origin)
 
 func _ready() -> void:
 	# setup arrays of drive elements and setup drive power
-	_drive_per_ray = enginePower / (left_drive_elements.size() + right_drive_elements.size())
-	print("Found %d track elements connected to vehicle. Each driveElement providing %.2f force each." % [left_drive_elements.size() + right_drive_elements.size(), _drive_per_ray])
+	_drive_per_ray = enginePower / (left_track.road_wheels.size() + right_track.road_wheels.size())
+	print("Found %d track elements connected to vehicle. Each driveElement providing %.2f force each." % [left_track.road_wheels.size() + right_track.road_wheels.size(), _drive_per_ray])
 
 func _physics_process(delta) -> void:
 	# calculate forward speed
